88f6565d764c3ab622f972294fc6daf619cf0e09
Voxin <mark@digitalice.nl>
2026-01-19 12:03:25 +0100

    Update EnemyMove.cs

--- My project/Assets/EnemyMove.cs
+++ My project/Assets/EnemyMove.cs
@@ -7,21 +7,25 @@ public class EnemyMove : MonoBehaviour
     public float posLeft;
     public float posRight;
 
-
     float nextPos;
+
+    [Header("Jump")]
+    public float jumpForce = 6f;
+    public float jumpEverySeconds = 1.5f;
+    private float jumpTimer;
 
     void Start()
     {
         enemyRb = GetComponent<Rigidbody2D>();
         nextPos = posRight;
+        jumpTimer = jumpEverySeconds;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if   (Mathf.Abs(transform.position.x - nextPos) < 0.05f)
-            {
-            if(nextPos == posRight)
+        if (Mathf.Abs(transform.position.x - nextPos) < 0.05f)
+        {
+            if (nextPos == posRight)
             {
                 nextPos = posLeft;
             }
@@ -30,8 +34,7 @@ public class EnemyMove : MonoBehaviour
                 nextPos = posRight;
             }
         }
-        
-        
+
         if (nextPos == posRight)
         {
             enemyRb.linearVelocityX = 1;
@@ -40,6 +43,13 @@ public class EnemyMove : MonoBehaviour
         {
             enemyRb.linearVelocityX = -1;
         }
-        
+
+        // ---- jumping every X seconds ----
+        jumpTimer -= Time.deltaTime;
+        if (jumpTimer <= 0f)
+        {
+            enemyRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpTimer = jumpEverySeconds;
+        }
     }
 }
